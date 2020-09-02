4f72ab8ecf82b473fe3bbe596a817a65df2e66da
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-09-02 11:48:28 -0600

    Update test to async

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -3,10 +3,14 @@ import App from './App'
 import { render, screen, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom'
 
+import { getSongs, postSong } from '../../helpers/apiCalls.js'
+jest.mock('../../helpers/apiCalls.js')
+
 describe('App Component', () => {
 
-	it('Should allow a user to add a new song', () => {
-		render(<App />)
+	it('Should allow a user to add a new song', async () => {
+		getSongs.mockResolvedValueOnce([])
+		const { findByRole, findByText } = render(<App />)
 
 		const songInputLabel = screen.getByLabelText(/song name:/i)
 		const artistInputLabel = screen.getByLabelText(/artist name:/i)
@@ -15,10 +19,18 @@ describe('App Component', () => {
 		fireEvent.change((artistInputLabel), { target: { value: 'Khalid and Casey' } })
 		fireEvent.change((linkInputLabel), { target: { value: 'link to reacc attac' } })
 		const addSongButton = screen.getByRole('button', {name: /add song!/i})
+
+		postSong.mockResolvedValueOnce({
+				'songName': 'reacc attac',
+				'artistName': 'khalid and casey',
+				'link': 'www.youtube.com/reaccattac',
+				'id': 1
+		})
 		fireEvent.click(addSongButton)
-		const newSongName = screen.getByRole('heading', {name: /reacc attac/i})
-		const newArtistName = screen.getByRole('heading', {name: /khalid and casey/i})
-		const newLink = screen.getByText(/listen to reacc attac here!/i)
+
+		const newSongName = await findByRole('heading', {name: /reacc attac/i})
+		const newArtistName = await findByRole('heading', {name: /khalid and casey/i})
+		const newLink = await findByText(/listen to reacc attac here!/i)
 
 		expect(newSongName).toBeInTheDocument()
 		expect(newArtistName).toBeInTheDocument()
