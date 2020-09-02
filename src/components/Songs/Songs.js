1e281c61579a2c0d2c7083962434ecce5b492331
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-09-02 09:44:34 -0600

    Write Songs component to render multiple SongCard components

---
+++ src/components/Songs/Songs.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Songs.css'
+
+const Songs = ({ allSongs }) => {
+	const songList = allSongs.map(song => {
+		return (
+			<SongCard
+				songName={song.songName}
+				artistName={song.artistName}
+				link={song.link}
+				id={song.id}
+				key={song.id}
+			/>
+		)
+	})
+
+	return (
+		<section className='Songs-container'>
+			<h2>All Songs</h2>
+			{songList}
+		</section>
+	)
+}
+
+export default Songs
