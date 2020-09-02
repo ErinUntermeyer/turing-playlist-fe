26601a034a5fc349f0f9aa1e950c7e05765f1557
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-09-02 11:02:23 -0600

    Write test that input fields capture the value

--- src/components/Form/Form.test.js
+++ src/components/Form/Form.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Form from './Form'
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom'
 
 describe('Form Component', () => {
@@ -8,13 +8,27 @@ describe('Form Component', () => {
 	it('Should display a form to add a new song', () => {
 		render(<Form />)
 
-		const songInput = screen.getByLabelText(/song name:/i)
-		const artistInput = screen.getByLabelText(/artist name:/i)
-		const linkInput = screen.getByLabelText(/link:/i)
+		const songInputLabel = screen.getByLabelText(/song name:/i)
+		const artistInputLabel = screen.getByLabelText(/artist name:/i)
+		const linkInputLabel = screen.getByLabelText(/link:/i)
+
+		expect(songInputLabel).toBeInTheDocument()
+		expect(artistInputLabel).toBeInTheDocument()
+		expect(linkInputLabel).toBeInTheDocument()
+	})
+
+	it('Should capture the value from the input fields', () => {
+		render(<Form />)
+
+		const songInputLabel = screen.getByLabelText(/song name:/i)
+		const artistInputLabel = screen.getByLabelText(/artist name:/i)
+		fireEvent.change((songInputLabel), {target: {value: 'LALALA!'}})
+		fireEvent.change((artistInputLabel), {target: {value: 'Howard and Finn'}})
+		const songInput = screen.getByDisplayValue(/lalala!/i)
+		const artistInput = screen.getByDisplayValue(/howard and finn/i)
 
 		expect(songInput).toBeInTheDocument()
 		expect(artistInput).toBeInTheDocument()
-		expect(linkInput).toBeInTheDocument()
 	})
 
 })
