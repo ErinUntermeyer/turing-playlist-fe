23de9986ed7b5d607f1117c7091ffa28fe2cfa42
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-09-02 09:29:40 -0600

    Write componentDidMount method to set state with song data

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,32 @@
-import React, { Component } from 'react';
-import './App.css';
-import SongController from '../SongController/SongController';
+import React, { Component } from 'react'
+import './App.css'
+import SongController from '../SongController/SongController'
+import { getSongs } from '../../helpers/apiCalls'
 
 class App extends Component {
   constructor() {
-    super();
+    super()
     this.state = {
       songQueue: []
     }
-  }
+	}
+	
+	componentDidMount() {
+		getSongs()
+			.then(data => {
+				this.setState({ songQueue: data })
+				console.log(this.state.songQueue)
+			})
+	}
 
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
+      <div className='App'>
+        <header className='App-header'>
           <h1>Turing Playlist</h1>
         </header>
-        <div className="App-background">
+        <div className='App-background'>
           <main>
           </main>
         </div> 
@@ -26,4 +35,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default App
