5719354d69625aa179b59beedfdf95394a720004
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-09-02 10:43:31 -0600

    Write test suite for SongCard

---
+++ src/components/SongCard/SongCard.test.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import SongCard from './SongCard'
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+
+describe('SongCard Component', () => {
+
+	it('Should display an individual song card', () => {
+
+	})
+
+})
