ddb376e6ba443171eb8fb29f72fab1f3fa6c0722
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-09-02 10:02:18 -0600

    Add form to render with inputs for song, artist and link

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -10,7 +10,35 @@ class Form extends Component {
 	}
 
 	render() {
-
+		return (
+			<form>
+				<h2>Add A New Song!</h2>
+				<label htmlFor='song'>Song Name:</label>
+				<input
+					className='song-input'
+					type='text'
+					id='song'
+					value={this.state.song}
+					onChange={(event) => this.handleInput(event)}
+				/>
+				<label htmlFor='artist'>Artist Name:</label>
+				<input
+					className='artist-input'
+					type='text'
+					id='artist'
+					value={this.state.artist}
+					onChange={(event) => this.handleInput(event)}
+				/>
+				<label htmlFor='link'>Link:</label>
+				<input
+					className='link-input'
+					type='text'
+					id='link'
+					value={this.state.link}
+					onChange={(event) => this.handleInput(event)}
+				/>
+			</form>
+		)
 	}
 }
 
