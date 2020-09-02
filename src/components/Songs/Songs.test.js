96d30839bfc8318d9456388dc2572e61a00dda82
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-09-02 10:42:08 -0600

    Write test that Songs displays a list of songs when rendered

---
+++ src/components/Songs/Songs.test.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import Songs from './Songs'
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+
+describe('Songs Component', () => {
+	let allSongs
+
+	beforeEach(() => {
+		allSongs = [
+			{
+				'songName': 'Cool Beans',
+				'artistName': 'The Beans',
+				'link': 'link to cool beans',
+				'id': 1
+			},
+			{
+				'songName': 'You Are Crushing It',
+				'artistName': 'Affirmations',
+				'link': 'link to you are crushing it',
+				'id': 2
+			}
+		]
+	})
+
+	it('Should display a list of songs', () => {
+		render(
+			<Songs
+				allSongs={allSongs}
+			/>
+		)
+
+		const songName1 = screen.getByText(/cool beans/i)
+		const songName2 = screen.getByText(/you are crushing it/i)
+		const artistName1 = screen.getByText(/the beans/i)
+		const artistName2 = screen.getByText(/affirmations/i)
+		const link1 = screen.getByText(/link to cool beans/i)
+		const link2 = screen.getByText(/link to you are crushing it/i)
+
+		expect(songName1).toBeInTheDocument()
+		expect(songName2).toBeInTheDocument()
+		expect(artistName1).toBeInTheDocument()
+		expect(artistName2).toBeInTheDocument()
+		expect(link1).toBeInTheDocument()
+		expect(link2).toBeInTheDocument()
+	})
+
+})
