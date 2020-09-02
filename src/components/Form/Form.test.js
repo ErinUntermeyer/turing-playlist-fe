0d17837347da974fc13cbd11ec338c4a994fd79e
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-09-02 10:27:06 -0600

    Set up Form test suite

---
+++ src/components/Form/Form.test.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Form from './Form'
+import { render } from '@testing-library/react'
+import '@testing-library/jest-dom'
+
+describe('Form Component', () => {
+
+	it('Should ', () => {
+		
+	})
+
+})
