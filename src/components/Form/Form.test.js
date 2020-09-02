37ddd152ad56638547477d2ac05151a2041586d8
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-09-02 10:34:02 -0600

    Write test that Form displays inputs upon render

--- src/components/Form/Form.test.js
+++ src/components/Form/Form.test.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import Form from './Form'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
 
 describe('Form Component', () => {
 
-	it('Should ', () => {
-		
+	it('Should display a form to add a new song', () => {
+		render(<Form />)
+
+		const songInput = screen.getByLabelText(/song name:/i)
+		const artistInput = screen.getByLabelText(/artist name:/i)
+		const linkInput = screen.getByLabelText(/link:/i)
+
+		expect(songInput).toBeInTheDocument()
+		expect(artistInput).toBeInTheDocument()
+		expect(linkInput).toBeInTheDocument()
 	})
 
 })
